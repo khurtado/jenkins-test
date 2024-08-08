1f4380cffd9d5b5dea0563f529f262bb5439869c
Dennis Lee <dylee@fnal.gov>
2024-08-08 11:39:44 -0500

    added print statements in CompareTests.py

--- docker/wmcore-dev/ContainerScripts/CompareTests.py
+++ docker/wmcore-dev/ContainerScripts/CompareTests.py
@@ -40,6 +40,8 @@ for kind, directory in [('base', './MasterUnitTests/'), ('test', './LatestUnitTe
             else:
                 testResults[testName] = {kind: tc.result}
 
+print(testResults)
+
 # Generate a Github report of any changes found
 
 issueID, mode = None, None
@@ -63,6 +65,7 @@ failed = False
 errorConditions = ['error', 'failure']
 
 for testName, testResult in sorted(testResults.items()):
+    print(f'{testName}: {testResult}')
     if 'base' in testResult and 'test' in testResult and testName in unstableTests:
         if testResult['base'] != testResult['test']:
             changed = True
