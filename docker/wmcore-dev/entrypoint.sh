596369cc790978f0a6195cef7eeebf82ae33d5d8
Dennis Lee <dylee@fnal.gov>
2024-08-14 08:56:36 -0500

    wmcore-dev: Remove git clone in Dockerfile, git clone conditional in entrypoint.sh

--- docker/wmcore-dev/entrypoint.sh
+++ docker/wmcore-dev/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# give proper permissions to
+# add WMCore repository, if in Jenkins
+if [[ -z "${BUILD_ID}" ]]; then
+    echo "No BUILD_ID set"
+else
+    echo "BUILD_ID set, cloning dmwm/WMCore"
+    git clone https://github.com/dmwm/WMCore
+fi
+
+# give proper permissions to home directory
 chown -R ${MY_ID}:${MY_GROUP} /home/cmsbld
 
 USERN=$(id -un ${MY_ID})
@@ -11,7 +19,6 @@ pushd /home/cmsbld
 
 # clone jenkins-test scripts
 git clone https://github.com/d-ylee/jenkins-test
-
 popd
 
 $@
