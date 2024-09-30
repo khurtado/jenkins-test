d19170a5b2503b7caa26267d85c7102f986d69c6
Kenyi Hurtado <khurtado@nd.edu>
2024-09-30 11:00:40 -0400

    Track github pull request id, fix docker compose ownership and add debugging lines

--- WMCore-Test-Base/setup-env.sh
+++ WMCore-Test-Base/setup-env.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# clean workspace
+rm -rf $WORKSPACE/admin $WORKSPACE/certs $WORKSPACE/artifacts
+rm -rf $WORKSPACE/srv/{couchdb,mariadb,wmagent}  
+
 # make directories
 mkdir -p $WORKSPACE/admin/wmagent $WORKSPACE/admin/mariadb $WORKSPACE/certs $WORKSPACE/artifacts
 mkdir -p $WORKSPACE/srv/couchdb/${COUCH_TAG}/install
@@ -19,6 +23,8 @@ cp /home/cmsbld/.globus/dmwm-mariadb.tmpl $WORKSPACE/admin/mariadb/MariaDB.secre
 cp /home/cmsbld/.globus/*.pem $WORKSPACE/certs
 mv $WORKSPACE/certs/usercert.pem $WORKSPACE/certs/servicecert.pem
 mv $WORKSPACE/certs/userkey.pem $WORKSPACE/certs/servicekey.pem
+chmod 600 $WORKSPACE/certs/servicecert.pem
+chmod 400 $WORKSPACE/certs/servicekey.pem
 
 # fix some incorrect information in secrets files
 # change to correct CouchDB port
